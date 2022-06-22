c864b95b815abfeaf3564188020edd2d8f3efa92
Jan Bassen <jan.d.bassen@gmail.com>
2022-06-22 11:59:18 +0200

    Adjust conda build variables

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,7 @@ if [ `uname` = "Linux" ]; then
     # there is a problem with NCSUopt when compiled with -fopenmp
     # so set the fflags manually:
     FFLAGS="-march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O2 -ffunction-sections -pipe"
+    # implicit variables in Fortran code exclude the use of conda's environment variables
 fi
 
 if [ $mpi == "nompi" ]; then
@@ -20,14 +21,19 @@ fi
 echo "DAKOTA_HAVE_MPI=$DAKOTA_HAVE_MPI"
 
 if [ ${build_type} == "Debug" ]; then
-    env | grep DEBUG
-    export DFFLAGS=${DEBUG_FFLAGS}
-    export FORTRANFLAGS=${DEBUG_FORTRANFLAGS}
-    export CXXFLAGS=${DEBUG_CXXFLAGS}
-    export CPPFLAGS=${DEBUG_CPPFLAGS}
-    export CFLAGS=${DEBUG_CFLAGS}
+    FFLAGS=$(echo ${FFLAGS} | sed "s:-O2 ::g")
+#   FORTRANFLAGS=$(echo ${DEBUG_FORTRANFLAGS} | sed "s:-O2 ::")
+    CXXFLAGS=$(echo ${DEBUG_CXXFLAGS} | sed 's:-fvisibility-inlines-hidden ::')
+    CPPFLAGS=$(echo ${DEBUG_CPPFLAGS} | sed 's:-fvisibility-inlines-hidden ::')
+    CFLAGS=${DEBUG_CFLAGS}
+    LDFLAGS=$(echo $LDFLAGS | sed 's:-Wl,-O2 ::')
+    echo "+FFLAGS=$FFLAGS"
+    echo "+FORTRANFLAGS=$FORTRANFLAGS"
+    echo "+CXXFLAGS=$CXXFLAGS"
+    echo "+CPPFLAGS=$CPPFLAGS"
+    echo "+CFLAGS=$CFLAGS"
+    echo "+LDFLAGS=$LDFLAGS"
 fi
-
 
 cmake -D CMAKE_BUILD_TYPE:STRING=${build_type} \
       -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
